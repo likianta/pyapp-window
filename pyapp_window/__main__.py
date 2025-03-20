bf96593822eb1bd25d1be246bed336fa128031a4
Likianta <likianta@foxmail.com>
2025-03-20 17:24:36 +0800

    tweak entrance args

--- pyapp_window/__main__.py
+++ pyapp_window/__main__.py
@@ -7,31 +7,35 @@ from .opener import open_window
 
 @cli.cmd()
 def launch(
-    # main params
+    # primary params
     url: str = None,
     port: int = None,
     # secondary params
+    backend: str = None,
     host: str = None,
+    icon: str = None,
+    pos: str = 'center',
     size: str = '1600x1200',
     title: str = 'PyApp Window',
-    pos: str = 'center',
-    backend: str = None,
 ) -> None:
     """
     kwargs:
+        title (-t):
+        url (-u):
         port (-p):
             if `url` is not specified but `port` is set, it will open a -
             localhost url.
         pos:
-            - 'center': in the screen center.
-            - '<x>,<y>', e.g. '100,200'.
+            - "center": in the screen center.
+            - `<x>,<y>`, e.g. "100,200".
                 you can use negative values to indicate "right/bottom -
                 margin to the edge of screen".
                 if x/y values are larger than screen size, it will be auto -
                 transformed to 10px to the edge of screen.
-            - '<x>,center' or 'center,<y>', e.g. '100,center'.
+            - `<x>,center` or `center,<y>`, e.g. "100,center".
         size (-s):
-            format: '<width>x<height>', e.g. '1600x1200'.
+            format: `<width>x<height>`, e.g. "1600x1200".
+        icon (-i): if given, must be ".ico" type.
     """
     assert url or port, 'either `url` or `port` must be set.'
     if ':' in pos or ',' in pos:
@@ -43,6 +47,7 @@ def launch(
     open_window(
         title,
         url,
+        icon=icon,
         host=host,
         port=port,
         pos=pos,
