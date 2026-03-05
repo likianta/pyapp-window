470ad196ad45dd2368d903cecc17b10c22191cda
Likianta <likianta@foxmail.com>
2026-03-05 15:07:39 +0800

    remove unused modules

--- pyapp_window/webview_window/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import util
-from .api import open_native_window
--- pyapp_window/webview_window/backend_implementations/webbrowser.py
+++
@@ -1,9 +0,0 @@
-import webbrowser
-
-from ._base import BaseApplication
-
-
-class Application(BaseApplication):
-    
-    def start(self) -> None:
-        webbrowser.open_new_tab(self.url)
